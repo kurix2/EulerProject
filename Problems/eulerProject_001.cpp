d8c0cd06855922ec531a64456a3e1928c0f103c6
Chris Lamb <kurix2@users.noreply.github.com>
2016-07-23 00:31:38 +0900

    Update eulerProject_001.cpp

--- Problems/eulerProject_001.cpp
+++ Problems/eulerProject_001.cpp
@@ -6,20 +6,38 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 */
 
-int problem1(int limit);
+int method1(int limit);
 
 int main() {
-  std::cout << problem1(1000) << std::endl;
+  std::cout << method1(1000) << std::endl;
+  std::cout << method2(1000) << std::endl;
+
   return 0;
 }
 
-int problem1(int limit) {
+// Brute Force
+int method1(int limit) {
   int sum = 0;
 
   for (int i = 0; i < limit; i++) {
     if (i%3 == 0 || i%5 == 0) {
       sum += i;
     }
+  }
+  return sum;
+}
+
+// Optimized 
+int method2(int limit) {
+  int sum = 0;
+  for (int i = 1; (3*i) < limit; i++) {
+    if ((3*i)%5 != 0) {
+      sum += (3*i);
+    }
+  }
+  
+  for (int j = 1; (5*j) < limit; j++) {
+        sum += (5*j);
   }
   return sum;
 }
